7aa287a59998eb19ebd9260b26b39746c906dbd7
Eric-Li-nk <73618843+Eric-Li-nk@users.noreply.github.com>
2023-04-23 23:48:24 +0200

    added Resolution options

--- Assets/Scripts/StartMenuHelper.cs
+++ Assets/Scripts/StartMenuHelper.cs
@@ -1,10 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenuHelper : MonoBehaviour
 {
+
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+    }
 
     public void Play()
     {
@@ -19,5 +30,10 @@ public class StartMenuHelper : MonoBehaviour
     public void FullScreen()
     {
         Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void SetResolution(int index)
+    {
+        Screen.SetResolution(resolutions[index].width, resolutions[index].height, Screen.fullScreenMode);
     }
 }
