bf00b93e83f87f9417e027517a2557cff635b127
Diallo Amidou <diallo.amidou24@yahoo.fr>
2023-06-02 15:56:15 +0200

    canva bonus + navmesh des bots

--- Assets/Scripts/PlayerNavMesh.cs
+++ Assets/Scripts/PlayerNavMesh.cs
@@ -6,17 +6,55 @@ using UnityEngine.AI;
 
 public class PlayerNavMesh : MonoBehaviour
 {
-    [SerializeField] private Transform movePositionTransform;
+    //[SerializeField] private Transform movePositionTransform;
+    [SerializeField] private List<Transform> checkpointTransforms;
+    [SerializeField] private bool loopCircuit = true;
 
     private NavMeshAgent navMeshAgent;
+    private int currentCheckpointIndex;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+    
+        if (loopCircuit)
+        {
+            currentCheckpointIndex = 0;
+        }
+        else
+        {
+            currentCheckpointIndex = checkpointTransforms.Count - 1;
+        }
     }
 
     private void Update()
     {
-        navMeshAgent.destination = movePositionTransform.position;
+        if (checkpointTransforms.Count == 0)
+        {
+            Debug.LogWarning("No checkpoints set!");
+            return;
+        }
+
+        SetDestinationToCurrentCheckpoint();
+
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            currentCheckpointIndex++;
+
+            if (currentCheckpointIndex >= checkpointTransforms.Count)
+            {
+                if (loopCircuit)
+                    currentCheckpointIndex = 0;
+                else
+                    currentCheckpointIndex = checkpointTransforms.Count - 1;
+            }
+
+            SetDestinationToCurrentCheckpoint();
+        }
+    }
+
+    private void SetDestinationToCurrentCheckpoint()
+    {
+        navMeshAgent.destination = checkpointTransforms[currentCheckpointIndex].position;
     }
 }
