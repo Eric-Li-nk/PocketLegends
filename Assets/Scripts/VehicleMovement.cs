3a974f8b854f31e7c7f80fd7d616f336fcbe1120
Eric-Li-nk <73618843+Eric-Li-nk@users.noreply.github.com>
2023-07-07 18:42:02 +0200

    reworked Vehicle lap tracker

--- Assets/Scripts/VehicleMovement.cs
+++ Assets/Scripts/VehicleMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class VehicleMovement : MonoBehaviour
@@ -9,61 +10,46 @@ public class VehicleMovement : MonoBehaviour
     [SerializeField] private Text SpeedText;
     public Transform centerOfMass;
     
-    public WheelCollider wheelColliderLeftFront;
-    public WheelCollider wheelColliderRightFront;
-    public WheelCollider wheelColliderLeftBack;
-    public WheelCollider wheelColliderRightBack;
-    
-    public Transform wheelLeftFront;
-    public Transform wheelRightFront;
-    public Transform wheelLeftBack;
-    public Transform wheelRightBack;
+    private Wheel[] wheels;
 
     [Header("Acceleration")]
     public float motorTorque = 100f;
     [Header("Maximum degree of turning")]
     public float maxSteer = 20f;
+    
+    public float ThrottleInput { get; private set; }
+    public float SteerInput { get; private set; }
+    
     private Rigidbody rb;
     
-    
+    private Vector2 movementInput = Vector2.zero;
 
     public void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        wheels = GetComponentsInChildren<Wheel>();
         // Keeps the vehicle from falling over
+        rb = GetComponent<Rigidbody>();
         rb.centerOfMass = centerOfMass.localPosition;
     }
 
     public void FixedUpdate()
     {
-        wheelColliderLeftBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        wheelColliderRightBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        wheelColliderLeftFront.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
-        wheelColliderRightFront.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
+        SteerInput = movementInput.x;
+        ThrottleInput = movementInput.y;
+        foreach (var wheel in wheels)
+        {
+            wheel.SteerAngle = SteerInput * maxSteer;
+            wheel.Torque = ThrottleInput * motorTorque;
+        }
     }
 
     public void Update()
     {
-        // Makes the wheels model turn
-        var pos = Vector3.zero;
-        var rot = Quaternion.identity;
-        
-        wheelColliderLeftFront.GetWorldPose(out pos, out rot);
-        wheelLeftFront.position = pos;
-        wheelLeftFront.rotation = rot;
-        
-        wheelColliderRightFront.GetWorldPose(out pos, out rot);
-        wheelRightFront.position = pos;
-        wheelRightFront.rotation = rot;
-        
-        wheelColliderLeftBack.GetWorldPose(out pos, out rot);
-        wheelLeftBack.position = pos;
-        wheelLeftBack.rotation = rot;
-        
-        wheelColliderRightBack.GetWorldPose(out pos, out rot);
-        wheelRightBack.position = pos;
-        wheelRightBack.rotation = rot;
-
         SpeedText.text = (rb.velocity.magnitude * 2.23693629f).ToString("0") + (" m/h");
+    }
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
     }
 }
