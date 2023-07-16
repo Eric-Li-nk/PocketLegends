e75441ffaa6ee4ea7f001cc2d50df6df2001539c
Diallo Amidou <diallo.amidou24@yahoo.fr>
2023-07-16 15:27:23 +0200

    amélioration script placement de lego
    Soumis à la gravité et disparais au bout d'un certain temps

--- Assets/Scripts/PlayerPower/CubePlacementAbility.cs
+++ Assets/Scripts/PlayerPower/CubePlacementAbility.cs
@@ -6,6 +6,7 @@ public class CubePlacementAbility : MonoBehaviour
     public GameObject cubePrefab; // Préfabriqué du cube à déposer
     public KeyCode placementKey = KeyCode.C; // Touche pour déposer le cube
     public float cooldownTime = 5f; // Temps d'attente en secondes
+    public float cubeLifetime = 3f; // Durée de vie du cube
 
     private bool canPlaceCube = true; // Variable pour vérifier si le cube peut être placé
 
@@ -20,7 +21,8 @@ public class CubePlacementAbility : MonoBehaviour
 
     private void PlaceCube()
     {
-        Instantiate(cubePrefab, transform.position - transform.forward, Quaternion.identity);
+        GameObject cube = Instantiate(cubePrefab, transform.position - transform.forward, Quaternion.identity);
+        StartCoroutine(DestroyCube(cube));
     }
 
     private IEnumerator StartCooldown()
@@ -30,5 +32,12 @@ public class CubePlacementAbility : MonoBehaviour
         yield return new WaitForSeconds(cooldownTime);
 
         canPlaceCube = true; // Réactiver la possibilité de placer le cube
+    }
+
+    private IEnumerator DestroyCube(GameObject cube)
+    {
+        yield return new WaitForSeconds(cubeLifetime);
+
+        Destroy(cube);
     }
 }
